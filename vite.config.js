29b4e529fe79f1d6e62bb1067be1ebeb8680ea9a
LupithaSotho <chicamala100888@gmail.com>
2025-11-03 17:39:03 -0600

    Corrige rutas base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Configuración para GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/montreal-atlacomulco/',
+  base: '/montreal-atlacomulco/', // 👈 nombre exacto de tu repositorio en GitHub
 })
