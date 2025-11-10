2f9a317a37235afea45143316e1b37c78f1ed9c3
LupithaSotho <chicamala100888@gmail.com>
2025-11-10 16:21:20 -0600

    Corrige base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/estatico-montreal/', // 👈 MUY IMPORTANTE (nombre del repo nuevo)
-})
+  base: "/estatica-montreal/", // 👈 nombre exacto del repo, SIN acento y con A
+});
