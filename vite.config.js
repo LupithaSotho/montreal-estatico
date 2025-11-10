e3d0afb517c06cc073200d18a26c0d2d6327b4c3
LupithaSotho <chicamala100888@gmail.com>
2025-11-10 16:06:35 -0600

    Sube compilación estática para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/montreal/', // 👈 MUY IMPORTANTE (nombre del repo nuevo)
+  base: '/estatico-montreal/', // 👈 MUY IMPORTANTE (nombre del repo nuevo)
 })
